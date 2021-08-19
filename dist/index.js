519791a3504eeedd755f6a0706f264582fc38000
maemae-dev <yana.sd.iml@gmail.com>
2021-08-19 11:37:30 +0900

    fetch milestone

--- dist/index.js
+++ dist/index.js
@@ -43,19 +43,32 @@ const github = __nccwpck_require__(438);
 const core = __nccwpck_require__(186);
 
 let test = async (version) => {
-	if (typeof version !== 'string') {
-		throw new Error('version not a string');
-	}
-	const token = core.getInput('token');
-	if (typeof token !== 'string') {
-		throw new Error('token is not valid');
-	}
+  if (typeof version !== "string") {
+    throw new Error("version not a string");
+  }
+  const token = core.getInput("token");
+  if (typeof token !== "string") {
+    throw new Error("token is not valid");
+  }
+  const octokit = github.getOctokit(token);
 
-  const octokit =	github.getOctokit(token);
+  const repo = await octokit.rest.repos.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
+  core.info(`${repo.data.name}`);
 
-	const repo = await octokit.rest.repos.get({owner: github.context.repo.owner, repo: github.context.repo.repo});
-	core.info(`${repo.data.name}`);
-
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.issues.listMilestones,
+    { owner: github.context.repo.owner, repo: github.context.repo.repo }
+  )) {
+    const milestones = response.data;
+    const filtered = milestones.filter((m) => m.title === version);
+    if (filtered.length !== 0) {
+      const milestone = filtered[0];
+      core.info(milestone.number);
+    }
+  }
 };
 
 module.exports = test;
--- milestone.js
+++ milestone.js
@@ -1,20 +1,33 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+const github = require("@actions/github");
+const core = require("@actions/core");
 
 let test = async (version) => {
-	if (typeof version !== 'string') {
-		throw new Error('version not a string');
-	}
-	const token = core.getInput('token');
-	if (typeof token !== 'string') {
-		throw new Error('token is not valid');
-	}
+  if (typeof version !== "string") {
+    throw new Error("version not a string");
+  }
+  const token = core.getInput("token");
+  if (typeof token !== "string") {
+    throw new Error("token is not valid");
+  }
+  const octokit = github.getOctokit(token);
 
-  const octokit =	github.getOctokit(token);
+  const repo = await octokit.rest.repos.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
+  core.info(`${repo.data.name}`);
 
-	const repo = await octokit.rest.repos.get({owner: github.context.repo.owner, repo: github.context.repo.repo});
-	core.info(`${repo.data.name}`);
-
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.issues.listMilestones,
+    { owner: github.context.repo.owner, repo: github.context.repo.repo }
+  )) {
+    const milestones = response.data;
+    const filtered = milestones.filter((m) => m.title === version);
+    if (filtered.length !== 0) {
+      const milestone = filtered[0];
+      core.info(milestone.number);
+    }
+  }
 };
 
 module.exports = test;
